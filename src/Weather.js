6e49d24782579f7bdda633d8a36561144781e1aa
I an <ydh8485@gmail.com>
2021-04-28 02:04:41 +0900

    api test

--- src/Weather.js
+++ src/Weather.js
@@ -1,40 +1,43 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Weather = () => {
   const [lat, setLat] = useState("위도");
   const [lon, setLon] = useState("경도");
   const [data, setData] = useState(null);
+  const [temp, setTemp] = useState("");
+  const [place, setPlace] = useState("");
+  const [icon, setIcon] = useState("");
   const API_KEY = "6ec556521fe3cc745950e32ddf07139f";
 
-  const location = () => {
-    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+  useEffect(() => {
+    const getWeather = async () => {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${API_KEY}`);
+        setData(response.data);
+        // setData(JSON.stringify(response.data));
+      } catch (e) {
+        console.log(e);
+      }
+    };
     getWeather();
-  };
+  }, []);
 
-  const handleGeoSuccess = (position) => {
-    setLat(position.coords.latitude);
-    setLon(position.coords.longitude);
-  };
-
-  function handleGeoError() {
-    console.log("Can't access geo location");
-  }
-  // 무한 요청 해결할 것
-  const getWeather = () => {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then((response) => {
-      setData(response.data);
-    });
-  };
-
+  // 객체는 렌더링 불가, JSON.stringify(data)로 JSON문자열로 변환 후 렌더링 가능,
   return (
     <div>
-      {location()}
-      <p>위도 : {lat}</p>
-      <p>경도 : {lon}</p>
-      {JSON.stringify(data, null, 2)}
+      <div></div>
+      {/* {data && JSON.stringify(data.name)} */}
+      {/* {data} */}
+      <br />
+      {data && JSON.stringify(data.weather[0].main)}
+      <br />
+      {data && JSON.stringify(data.main.temp)}
+      {/* {temp} */}
+      {/* {icon} */}
     </div>
   );
 };
 
-export default Weather;
+// 컴포넌트의 props가 바뀌지 않았을때 리렌더링 방지
+export default React.memo(Weather);
