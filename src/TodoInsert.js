9193125d944c865d1886609edeb8cadc0c171643
DESKTOP-T0OAPSS\ydh84 <ydh8485@gmail.com>
2021-05-14 23:47:57 +0900

    Todo Component initial settings

---
+++ src/TodoInsert.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoInsert = () => {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default TodoInsert;
---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoItem = () => {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default TodoItem;
